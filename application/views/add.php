a74c5251fbf911ca01850ac37c7d96fae0b8ca24
Muhammad Ramdhan Aditya S <mramdhanass@gmail.com>
2023-05-05 10:04:50 +0700

    ok:

--- application/views/add.php
+++ application/views/add.php
@@ -46,34 +46,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 												<td style="width: 10%;" class="text-center ">
 													<a href="<?php echo base_url() . "detail/" . $d['id']; ?>" class="btn btn-primary btn-circle btn-sm"><i class="fas fa-eye"></i></a>
 													<button type="button" class="btn btn-warning btn-circle btn-sm" title="Edit Data"><i class="fas fa-edit"></i></button>
-													<a href="<?php echo base_url('delete/' . $d['id']); ?>" onclick="return confirm('Are you sure to delete this data employee ( <?= $d['project_name']; ?> ) ?');" class="btn btn-danger btn-circle btn-sm" data-popup="tooltip" data-placement="top" title="Hapus Data"><i class="fa fa-trash"></i></a>
+													<a href="<?php echo base_url('delete/' . $d['id']); ?>" onclick="return confirm('Are you sure to delete this data employee ( <?= $d['first_name']; ?> ) ?');" class="btn btn-danger btn-circle btn-sm" data-popup="tooltip" data-placement="top" title="Hapus Data"><i class="fa fa-trash"></i></a>
 												</td>
 											</tr>
 										<?php } ?>
 									</tbody>
 								</table>
-							</div>
-							<div class="modal fade" id="modal-tambah" tabindex="-1" role="dialog" aria-labelledby="modal-tambah-label" aria-hidden="true">
-								<div class="modal-dialog" role="document">
-									<div class="modal-content">
-										<div class="modal-header">
-											<h5 class="modal-title" id="modal-tambah-label">Add Data Employee</h5>
-											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-												<span aria-hidden="true">&times;</span>
-											</button>
-										</div>
-										<div class="modal-body">
-											<!-- Konten form untuk menambahkan data employee -->
-											<form>
-												<!-- Isi form di sini -->
-											</form>
-										</div>
-										<div class="modal-footer">
-											<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-											<button type="button" class="btn btn-primary">Save changes</button>
-										</div>
-									</div>
-								</div>
 							</div>
 						</div>
 					</div>
